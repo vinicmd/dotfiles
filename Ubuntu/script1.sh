42886cdadaea1deea79cf2c1f5ddaa1dd55eb91e
Vinicius Duarte <viniciusduarte@outlook.com>
2021-08-18 23:27:02 -0300

    Corrigindo ubuntu files

--- Ubuntu/script1.sh
+++ Ubuntu/script1.sh
@@ -5,10 +5,11 @@ sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-beta.list'
 rm -f packages.microsoft.gpg
+sudo add-apt-repository ppa:daniruiz/flat-remix
 
 sudo apt install apt-transport-https -y
 sudo apt update
-sudo apt install code git gnome-tweaks zsh tilix microsoft-edge-beta curl -y
+sudo apt install code git gnome-tweaks zsh tilix microsoft-edge-beta flat-remix flat-remix-gnome flat-remix-gtk curl -y
 
 sudo usermod --shell $(which zsh) $USER
 
